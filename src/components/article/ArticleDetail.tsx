5b5b0cf0b95d667cb0149a993a2c6f8d0929118f
yeppyshiba <100031060+yeppyshiba@users.noreply.github.com>
2022-04-11 15:10:30 +0900

    Update ArticleDetail.tsx

--- src/components/article/ArticleDetail.tsx
+++ src/components/article/ArticleDetail.tsx
@@ -2,7 +2,7 @@ import React, { FunctionComponent } from 'react';
 import { Helmet } from 'react-helmet';
 import { MDXRenderer } from "gatsby-plugin-mdx"
 import { Box, Container, Heading, HStack, Text } from '@chakra-ui/react';
-import { ArticlePageItemType } from '@/types';
+import { ChakraMdxProvider } from '@/components/common';
 
 type ArticleDetailProps = {
   data: {
@@ -51,7 +51,9 @@ const ArticleDetail: FunctionComponent<ArticleDetailProps> = ({ data: { mdx } })
             justifyContent="center"
             marginTop={{ base: '3', sm: '0' }}
           >
-            <MDXRenderer>{body}</MDXRenderer>
+            <ChakraMdxProvider>
+              <MDXRenderer>{body}</MDXRenderer>
+            </ChakraMdxProvider>
             <HStack marginTop="2" spacing="2" display="flex" alignItems="center">
               <Text>{date}</Text>
             </HStack>
