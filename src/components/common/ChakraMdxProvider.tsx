76bdd98d83867904b5b2fa0ece86b57e5f2bad1e
yeppyshiba <100031060+yeppyshiba@users.noreply.github.com>
2022-04-11 15:23:07 +0900

    Update ChakraMdxProvider.tsx

--- src/components/common/ChakraMdxProvider.tsx
+++ src/components/common/ChakraMdxProvider.tsx
@@ -16,8 +16,31 @@ import {
 } from '@chakra-ui/react';
 import { MDXProvider, MDXProviderComponents } from '@mdx-js/react';
 
+const Heading = (props: any) => {
+  const { children } = props;
+  const sizes = ['2xl', 'xl', 'lg', 'md', 'sm', 'xs'];
+  const level: number = props.level;
+
+  return (
+    <ChakraHeading
+      my={4}
+      as={`h${level}`}
+      size={sizes[level - 1]}
+      {...getCoreProps(props)}
+    >
+      {children}
+    </ChakraHeading>
+  );
+};
+
 const MdxProviderComponents: MDXProviderComponents = {
   p: (props: any) => <Text my={4} {...props} />,
+  h1: (props: any) => <Heading {...props} level={1} />,
+  h2: (props: any) => <Heading {...props} level={2} />,
+  h3: (props: any) => <Heading {...props} level={3} />,
+  h4: (props: any) => <Heading {...props} level={4} />,
+  h5: (props: any) => <Heading {...props} level={5} />,
+  h6: (props: any) => <Heading {...props} level={6} />,  
 };
 
 type ChakraMdxProviderProps = {
