43b1cacb8ad72e8db027791e8d92088406aa9c68
yeppyshiba <100031060+yeppyshiba@users.noreply.github.com>
2022-04-11 16:27:00 +0900

    Update ChakraMdxProvider.tsx

--- src/components/common/ChakraMdxProvider.tsx
+++ src/components/common/ChakraMdxProvider.tsx
@@ -16,6 +16,25 @@ import {
 } from '@chakra-ui/react';
 import { MDXProvider, MDXProviderComponents } from '@mdx-js/react';
 
+/*
+TODO
+- thematicBreak -> Thematic break
+- blockquote -> Blockquote
+- ul -> List
+- ol -> Ordered List
+- li -> List Item
+- table -> Table
+- tr -> Table Row
+- td/th -> Table Cell
+- code -> Code
+- em -> Emphasis
+- strong -> Strong
+- delete -> Delete
+- inlineCode -> Inline Code
+- a -> Link
+- img -> Image
+*/
+
 const Heading = (props: any) => {
   const sizes = ['2xl', 'xl', 'lg', 'md', 'sm', 'xs'];
   const level: number = props.level;
@@ -38,6 +57,8 @@ const MdxProviderComponents: MDXProviderComponents = {
   h4: (props: any) => <Heading level={4} {...props} />,
   h5: (props: any) => <Heading level={5} {...props} />,
   h6: (props: any) => <Heading level={6} {...props} />,
+  hr: (props: any) => <Divider {...props} />,
+  pre: (props: any) => <chakra.pre {...props} maxW="100%" overflowX="auto" />,
 };
 
 type ChakraMdxProviderProps = {
