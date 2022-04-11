a3a8934826b47c69b8fe476ccab0bf72231fcb19
yeppyshiba <100031060+yeppyshiba@users.noreply.github.com>
2022-04-11 15:29:06 +0900

    Update ChakraMdxProvider.tsx

--- src/components/common/ChakraMdxProvider.tsx
+++ src/components/common/ChakraMdxProvider.tsx
@@ -17,7 +17,6 @@ import {
 import { MDXProvider, MDXProviderComponents } from '@mdx-js/react';
 
 const Heading = (props: any) => {
-  const { children } = props;
   const sizes = ['2xl', 'xl', 'lg', 'md', 'sm', 'xs'];
   const level: number = props.level;
 
@@ -26,21 +25,19 @@ const Heading = (props: any) => {
       my={4}
       as={`h${level}`}
       size={sizes[level - 1]}
-      {...getCoreProps(props)}
-    >
-      {children}
-    </ChakraHeading>
+      {...props}
+    />
   );
 };
 
 const MdxProviderComponents: MDXProviderComponents = {
   p: (props: any) => <Text my={4} {...props} />,
-  h1: (props: any) => <Heading {...props} level={1} />,
-  h2: (props: any) => <Heading {...props} level={2} />,
-  h3: (props: any) => <Heading {...props} level={3} />,
-  h4: (props: any) => <Heading {...props} level={4} />,
-  h5: (props: any) => <Heading {...props} level={5} />,
-  h6: (props: any) => <Heading {...props} level={6} />,  
+  h1: (props: any) => <Heading level={1} {...props} />,
+  h2: (props: any) => <Heading level={2} {...props} />,
+  h3: (props: any) => <Heading level={3} {...props} />,
+  h4: (props: any) => <Heading level={4} {...props} />,
+  h5: (props: any) => <Heading level={5} {...props} />,
+  h6: (props: any) => <Heading level={6} {...props} />,
 };
 
 type ChakraMdxProviderProps = {
