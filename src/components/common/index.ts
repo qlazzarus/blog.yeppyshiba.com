1c6345ba9b9b595b71a6f3c69c19b1dc213dff14
yeppyshiba <100031060+yeppyshiba@users.noreply.github.com>
2022-04-11 15:08:01 +0900

    Update index.ts

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,3 +1,4 @@
+export { default as ChakraMdxProvider } from './ChakraMdxProvider';
 export { default as Footer } from './Footer';
 export { default as Header } from './Header';
 export { default as Layout } from './Layout';
