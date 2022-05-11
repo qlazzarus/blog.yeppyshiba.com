549a4e1a0179298808296d834887e613cfa4f647
shiba <shiba@yeppyshiba.com>
2022-05-11 19:17:10 +0900

    adding modal state init

--- src/hooks/useModalState.ts
+++ src/hooks/useModalState.ts
@@ -5,11 +5,13 @@ const useModalState = (id: string) => {
     const context = useContext(ModalStateContext);
     if (!context) {
         // TODO throw error
+        return null;
     }
 
     const { modals } = context as ModalState;
     if (!modals) {
         // TODO throw error
+        return null;
     }
 
     return modals[id];
