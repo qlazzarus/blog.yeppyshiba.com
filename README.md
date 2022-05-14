b437610f1863b29ae84486c794e85525735e680a
monoless <ecst@monolife.co.kr>
2022-05-14 21:00:43 +0900

    enhanced article card

--- README.md
+++ README.md
@@ -3,5 +3,4 @@
 - [ ] adding about / https://www.gatsbyjs.com/docs/creating-and-modifying-pages/
 - [ ] adding categories link / index
 - [ ] adding tag cloud link / index
-- [ ] adding footnode as modal
 - [ ] adding image as carousel / https://codesandbox.io/s/dd8vn
--- src/components/article/ArticleList.tsx
+++ src/components/article/ArticleList.tsx
@@ -1,13 +1,28 @@
 import React, { FunctionComponent } from 'react';
 import { Link as GatsbyLink } from 'gatsby';
-import { Box, Button, Container, Heading, HStack, Image, Link, SpaceProps, Stack, Tag, Text, useColorModeValue } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  Container,
+  Divider,
+  Flex,
+  Heading,
+  HStack,
+  Image,
+  SpaceProps,
+  Stack,
+  Tag,
+  Text,
+  useColorModeValue,
+  VStack
+} from '@chakra-ui/react';
 import { kebabCase } from 'lodash';
 import { MathUtil } from '@/utils';
 import { ArticleListItemType } from '@/types';
 
 const defaultImages = [
   '/images/cards/pexels-olia-danilevich-4974915.jpg',
-  '/images/header/wp7317693-jeju-wallpapers.jpg'
+  '/images/header/wp7317693-jeju-wallpapers.jpg',
 ];
 
 interface ArticleListProps {
@@ -20,7 +35,7 @@ interface ArticleEntryProps {
 
 interface TagProps {
   tags: Array<string>;
-  marginTop?: SpaceProps['marginTop']
+  marginTop?: SpaceProps['marginTop'];
 }
 
 interface AuthorProps {
@@ -29,48 +44,51 @@ interface AuthorProps {
 }
 
 const Tags: FunctionComponent<TagProps> = ({ tags, marginTop }) => {
- return (
-  <HStack 
-    spacing={2}
-    marginTop={marginTop}
-  >
-    {tags.map(tag => {
-      return (
-        <Tag size={'md'} variant="solid" key={tag}>
-          {tag}
-        </Tag>
-      );
-    })}
-  </HStack>
- );  
-}
+  return (
+    <HStack spacing={2} marginTop={marginTop}>
+      {tags.map((tag) => {
+        return (
+          <Tag size={'md'} variant="solid" key={tag}>
+            {tag}
+          </Tag>
+        );
+      })}
+    </HStack>
+  );
+};
 
-const ArticleEntry: FunctionComponent<ArticleEntryProps> = ({ entry: { slug, frontmatter: { category, date, summary, image, title, tags } } }) => {
+const ArticleEntry: FunctionComponent<ArticleEntryProps> = ({
+  entry: {
+    slug,
+    frontmatter: { category, date, summary, image, title, tags },
+  },
+}) => {
   const articleImage = image || MathUtil.getRandomValue(defaultImages);
   const dateObj = new Date(date);
 
   return (
-    <Box
-      maxW={'445px'}
-      w={'full'}
-      bg={useColorModeValue('white', 'gray.900')}
+    <Flex
+      bgColor={useColorModeValue('white', 'gray.900')}
+      borderRadius={'md'}
       boxShadow={'2xl'}
-      rounded={'md'}
-      p={6}
-      overflow={'hidden'}
+      flexDir={{ base: 'column', md: 'row' }}
+      maxW={{ base: 'sm', md: 'full' }}
+      overflow={'auto'}
+      w={'full'}
     >
-      <Box
-        bg={'gray.100'}
-        mt={-6}
-        mx={-6}
-        mb={6}
-        pos={'relative'}>
-        <Image 
-          src={articleImage} 
-          alt={title} 
-        />
-      </Box>
-      <Stack>
+      <Image
+        alt={title}
+        maxH={'xs'}
+        maxW={{ md: 'sm' }}
+        objectFit={'cover'}
+        objectPosition={'center'}
+        src={articleImage}
+        w={'full'}
+      />
+      <Stack p={4} w={'full'}>
+        <Heading as="h3" size="md" color={useColorModeValue('gray.700', 'white')}>
+          {title}
+        </Heading>
         {category && (
           <Text
             color={'green.500'}
@@ -79,79 +97,56 @@ const ArticleEntry: FunctionComponent<ArticleEntryProps> = ({ entry: { slug, fro
             fontSize={'sm'}
             letterSpacing={1.1}
             as={GatsbyLink}
-            to={`/category/${kebabCase(category)}`}>
+            to={`/category/${kebabCase(category)}`}
+          >
             {category}
           </Text>
         )}
-        <Heading
-          color={useColorModeValue('gray.700', 'white')}
-          fontSize={'2xl'}
-          fontFamily={'body'}>
-          {title}
-        </Heading>
-        <Text color={'gray.500'}>
+        <Text fontSize={'sm'} fontWeight={'bold'}>
+          {dateObj.toLocaleDateString()}
+        </Text>
+        <Divider />
+        <Text flexGrow={1} fontSize={'sm'}>
           {summary}
         </Text>
+        <HStack justify={'flex-end'} py={4}>
+          {tags.map((tag) => (
+            <Tag size={'md'} variant="solid" key={tag} as={GatsbyLink} to={`/tag/${kebabCase(tag)}`}>
+              {tag}
+            </Tag>
+          ))}
+        </HStack>
+        <HStack justify={'flex-end'}>
+          <Button
+            bg={'green.400'}
+            color={'white'}
+            rounded={'xl'}
+            boxShadow={'0 5px 20px 0px rgb(72 187 120 / 43%)'}
+            _hover={{
+              bg: 'green.500',
+            }}
+            _focus={{
+              bg: 'green.500',
+            }}
+            as={GatsbyLink}
+            to={`/article/${slug}`}
+          >
+            Read more
+          </Button>
+        </HStack>
       </Stack>
-      <Stack 
-        mt={6}
-        direction={'row'}
-        align={'center'}>
-        <Stack 
-          width={'100%'}
-          direction={'row'}
-          spacing={0}
-          fontSize={'sm'}
-          align={'center'}
-          justifyContent={'space-between'}>
-          <Text color={'gray.500'}>
-            {dateObj.toLocaleDateString()}
-          </Text>
-          <Stack 
-            direction={'row'}
-            spacing={2}>
-            {tags.map(tag => (
-              <Tag 
-                size={'md'} 
-                variant="solid" 
-                key={tag}
-                as={GatsbyLink}
-                to={`/tag/${kebabCase(tag)}`}>
-                {tag}
-              </Tag>
-            ))}
-          </Stack>
-        </Stack>
-      </Stack>
-      <Button
-        mt={10}
-        w={'full'}
-        bg={'green.400'}
-        color={'white'}
-        rounded={'xl'}
-        boxShadow={'0 5px 20px 0px rgb(72 187 120 / 43%)'}
-        _hover={{
-          bg: 'green.500',
-        }}
-        _focus={{
-          bg: 'green.500',
-        }}
-        as={GatsbyLink}
-        to={`/article/${slug}`}>
-        Read more
-      </Button>
-    </Box>
+    </Flex>
   );
 };
 
 const ArticleList: FunctionComponent<ArticleListProps> = ({ entries }) => {
   return (
-    <Container 
-      maxW={'7xl'} 
-      p={"12"}>
-      {entries.map((entry) => (
-        <ArticleEntry entry={entry} key={entry.id} />
-      ))}
+    <Container as={VStack} maxW={'6xl'} px={[4, 8]} py={12} spacing={[8, 12]}>
+      <VStack spacing={[2, 4]} w={'full'}>
+        {entries.map((entry) => (
+          <ArticleEntry entry={entry} key={entry.id} />
+        ))}
+      </VStack>
     </Container>
   );
 };
