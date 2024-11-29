93d7f69eefc23fe29120643f0978e83f9a62000a
monoless <monoless@photowidget.net>
2024-11-29 18:24:59 +0900

    prepare for convert to nextjs

---
+++ .eslintrc.ts
@@ -0,0 +1,26 @@
+import { Linter } from 'eslint';
+
+const config: Linter.Config = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  extends: [
+    'next/core-web-vitals', // Next.js 기본 ESLint 설정
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier', // Prettier와 통합
+  ],
+  rules: {
+    // 프로젝트에 맞는 룰을 추가
+    'react/react-in-jsx-scope': 'off', // React 17+에서 JSX Import 불필요
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+  },
+};
+
+export default config;
---
+++ src/app/category/[slug]/page.tsx
@@ -0,0 +1,3 @@
+export default function Category() {
+    return <>hello category</>
+}
