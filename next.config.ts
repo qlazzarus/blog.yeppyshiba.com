1a2b03f473fb6cf1f4f3eec314587aa77b24d32a
monoless <monoless@photowidget.net>
2024-12-16 19:27:47 +0900

    appbar implements

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
     // build 파일을 외부 static으로 뽑을 떄 추가하는 옵션
     output: 'export',
+    images: {
+        unoptimized: true,
+    },
 };
 
 export default nextConfig;
--- src/components/ResponsiveAppBar.tsx
+++ src/components/ResponsiveAppBar.tsx
@@ -1,3 +1,8 @@
+'use client';
+
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import MenuIcon from '@mui/icons-material/Menu';
 import AppBar from '@mui/material/AppBar';
 import Avatar from '@mui/material/Avatar';
 import Box from '@mui/material/Box';
@@ -9,14 +14,13 @@ import MenuItem from '@mui/material/MenuItem';
 import Toolbar from '@mui/material/Toolbar';
 import Tooltip from '@mui/material/Tooltip';
 import Typography from '@mui/material/Typography';
+import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 
+// 이미지 경로를 실제 프로젝트에 맞게 수정
+
 const pages = [
-    {
-        title: 'Blog',
-        link: '/',
-    },
     {
         title: 'About',
         link: '/about',
@@ -36,29 +40,118 @@ const pages = [
 ];
 
 const ResponsiveAppBar = () => {
+    const [anchorElNav, setAnchorElNav] = React.useState(null);
+
+    const handleOpenNavMenu = (event: any) => {
+        setAnchorElNav(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+
     return (
         <AppBar position="static">
             <Container maxWidth="xl">
                 <Toolbar disableGutters>
-                    <Typography
-                        variant="h6"
-                        noWrap
-                        sx={{
-                            mr: 2,
-                            display: { xs: 'none', md: 'flex' },
-                            fontFamily: 'monospace',
-                            fontWeight: 700,
-                            letterSpacing: '.3rem',
-                            color: 'inherit',
-                            textDecoration: 'none',
-                        }}
-                    >
-                        <Link href="/" style={{ textDecoration: 'none', color: 'inherit' }}>
-                            Yeppyshiba Blog
-                        </Link>
-                    </Typography>
+                    {/* 메뉴 (Mobile) */}
+                    <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                        <IconButton
+                            size="large"
+                            aria-label="menu"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            onClick={handleOpenNavMenu}
+                            color="inherit"
+                        >
+                            <Image src={'/akita-inu.png'} alt="Yeppyshiba Blog" width={24} height={24} />
+                        </IconButton>
+                        <Menu
+                            id="menu-appbar"
+                            anchorEl={anchorElNav}
+                            anchorOrigin={{
+                                vertical: 'top',
+                                horizontal: 'left',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'left',
+                            }}
+                            open={Boolean(anchorElNav)}
+                            onClose={handleCloseNavMenu}
+                            sx={{
+                                display: { xs: 'block', md: 'none' },
+                            }}
+                        >
+                            <MenuItem onClick={handleCloseNavMenu}>
+                                <Typography textAlign="center">
+                                    <Link href={'/'} passHref>
+                                        {'Blog'}
+                                    </Link>
+                                </Typography>
+                            </MenuItem>
+                            {pages.map((page) => (
+                                <MenuItem key={page.title} onClick={handleCloseNavMenu}>
+                                    <Typography textAlign="center">
+                                        <Link href={page.link} passHref>
+                                            {page.title}
+                                        </Link>
+                                    </Typography>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
 
-                    <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}></Box>
+                    {/* 홈 로고 */}
+                    <Box sx={{ display: { xs: 'none', md: 'flex' }, alignItems: 'center', mr: 2 }}>
+                        <Link href="/" style={{ textDecoration: 'none', display: 'flex', alignItems: 'center' }}>
+                            <Image src={'/akita-inu.png'} alt="Yeppyshiba Blog" width={24} height={24} />
+                            <Typography variant="h6" noWrap component="div" sx={{ color: 'white' }} ml={1}>
+                                Yeppyshiba Blog
+                            </Typography>
+                        </Link>
+                    </Box>
+
+                    {/* 메뉴 (Desktop) */}
+                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                        {pages.map((page) => (
+                            <Link key={page.title} href={page.link} passHref>
+                                <Button
+                                    sx={{
+                                        my: 2,
+                                        color: 'white',
+                                        display: 'block',
+                                        textTransform: 'none',
+                                    }}
+                                >
+                                    {page.title}
+                                </Button>
+                            </Link>
+                        ))}
+                    </Box>
+
+                    {/* 외부 링크 */}
+                    <Box sx={{ flexGrow: 0 }}>
+                        <IconButton
+                            component="a"
+                            href="https://github.com/qlazzarus"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            color="inherit"
+                        >
+                            <GitHubIcon />
+                        </IconButton>
+                        <IconButton
+                            component="a"
+                            href="https://www.linkedin.com/in/yeppyshiba/"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            color="inherit"
+                        >
+                            <LinkedInIcon />
+                        </IconButton>
+                    </Box>
                 </Toolbar>
             </Container>
         </AppBar>
