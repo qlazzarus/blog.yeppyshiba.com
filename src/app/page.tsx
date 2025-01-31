ce2cf12ea6698342d61f86bed624a9819465b6f2
monoless <monoless@photowidget.net>
2025-01-31 15:05:05 +0900

    tailing slashes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,12 @@
+import generatePageMetadata from '@/seo';
+import type { Metadata } from 'next';
 import { redirect } from 'next/navigation';
+
+export const metadata: Metadata = generatePageMetadata({ 
+    title: 'Home', 
+    description: 'Explore posts on page 1', 
+    url: '/page'
+});
 
 export default function Page() {
     redirect('/page/1');
