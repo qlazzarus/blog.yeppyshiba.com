17de3d9782ef292861896f6b4abcaa4f35ee5b36
monoless <monoless@ibleaders.co.kr>
2026-01-09 10:55:13 +0900

    feature :tada: manifesto in 2026

--- src/content.config.ts
+++ src/content.config.ts
@@ -8,7 +8,7 @@ const blog = defineCollection({
         z.object({
             title: z.string(),
             date: z.coerce.date(),
-            category: z.enum(['review', 'coding']),
+            category: z.enum(['review', 'coding', 'essay']),
             summary: z.string(),
             // optional tags array
             tags: z.array(z.string()).optional(),
